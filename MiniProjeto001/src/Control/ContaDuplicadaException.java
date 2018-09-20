d0154b07dee4ba4804facca20b57fdfb63ce0cd3
cleefsouza <a.cleef.souza@gmail.com>
2018-09-20 19:40:06 -0300

    Exception ContaDuplicada

---
+++ MiniProjeto001/src/Control/ContaDuplicadaException.java
@@ -0,0 +1,9 @@
+package Control;
+
+public class ContaDuplicadaException extends Exception{
+	// Construtor
+	public ContaDuplicadaException(String msg) {
+		super(msg); // Mensagem enviada para superclasse
+	}
+
+}
